406665f262dd5579e79babc6af51124b52736324
Philip Okeniyi <okeniyiphilip@gmail.com>
2025-02-18 12:38:10 +0100

    react rendring

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Game from "./Game";
 function App() {
+  const username="youngsoft"
   return (
     <>
-      <h1>This is Philip testing importing and exporting</h1>
+      <h1>This is Philip testing {6 + 9 } importing and exporting</h1>
+      <h1>This is {username} testing importing and exporting</h1>
       <Game />
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import Game from './Game.jsx'
 
+
+
 createRoot(document.getElementById('root')).render(
   <>
   <Game />
